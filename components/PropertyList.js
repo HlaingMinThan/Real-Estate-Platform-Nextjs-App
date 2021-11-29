2af2aadfdda25ad1c2c8c2ee34a4298ad6ce6702
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-11-29 11:51:50 +0630

    property alignment

--- components/PropertyList.js
+++ components/PropertyList.js
@@ -2,7 +2,7 @@ import { Flex } from "@chakra-ui/react";
 import Property from "./Property";
 export default function ({ properties }) {
   return (
-    <Flex flexWrap="wrap">
+    <Flex flexWrap="wrap" justifyContent="center">
       {properties.map((property) => (
         <Property property={property} key={property.id} />
       ))}
